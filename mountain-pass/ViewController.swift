5236f42a83f5f6f06d2d448da61ac56bd5cb7471
Vasily Maslov <44157132+Stubbs221@users.noreply.github.com>
2022-05-15 23:07:28 +0300

    Initial Commit

---
+++ mountain-pass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mountain-pass
+//
+//  Created by Vasily on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
